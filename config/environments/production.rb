f082d689a9fc80317ec6cc6797b338f2f85ac976
vinit-initcoders <vinit@initcoders.com>
2016-06-08 17:58:34 +0530

    implimented devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Note to set your actual host name
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } #Requir for heroku
+
 end
