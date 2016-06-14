eacea3095b65e3e2da205a0a1f60e9ae92d231fb
vinit-initcoders <vinit@initcoders.com>
2016-06-14 11:23:21 +0530

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	 has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-end
+     validates :image, presence: true
+     validates :description, presence: true
+  end
